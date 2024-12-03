32f9138f308c871ff86feaffd3310f6ba224d175
Rubens Moraes Ruegenberg <rubensmoraesruegenberg@outlook.com>
2024-12-03 16:10:34 -0300

    Ajuste api Get Users id

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/GetUserRequestProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings;
+
+public class GetUserRequestProfile : Profile
+{
+    public GetUserRequestProfile()
+    {
+        CreateMap<GetUserResult,GetUserResponse>();
+    }
+}
